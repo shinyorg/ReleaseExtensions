6a4ff643ab7d838fbe778d2c4798e6d34fe0feb3
Allan Ritchie <allan.ritchie@gmail.com>
2021-12-21 18:58:20 -0500

    Add project files.

---
+++ src/Shiny.DocFx.Extensions/NugetShieldInlineRule.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.DocAsCode.MarkdownLite;
+using System.Text.RegularExpressions;
+
+
+namespace Shiny.DocFx.Extensions
+{
+    public class NugetShieldInlineRule : IMarkdownRule
+    {
+        static readonly Regex regex = new Regex(@"^\[EnvVar:(\w+?)\]", RegexOptions.Compiled);
+
+
+        public string Name => "NugetShieldToken";
+
+        public IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
+        {
+            var match = regex.Match(context.CurrentMarkdown);
+            if (match.Length == 0)
+                return null;
+
+            var packageName = match.Groups[1].Value;
+            if (packageName == null)
+                return null;
+
+            var text = Utils.ToNugetShield(packageName);
+
+            // 'eat' the characters of the current markdown token so they aren't re-processed
+            var sourceInfo = context.Consume(match.Length);
+
+            // return a docfx token that just returns the text passed to it
+            return new MarkdownTextToken(this, parser.Context, text, sourceInfo);
+        }
+    }
+}
---
+++ src/Shiny.DocFx.Extensions/ShinyDfmEngineCustomizer.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.DocAsCode.Dfm;
+using System.Collections.Generic;
+using System.Composition;
+
+
+namespace Shiny.DocFx.Extensions
+{
+    [Export(typeof(IDfmEngineCustomizer))]
+    public class ShinyDfmEngineCustomizer : IDfmEngineCustomizer
+    {
+        public void Customize(DfmEngineBuilder builder, IReadOnlyDictionary<string, object> parameters)
+        {
+            builder.InlineRules = builder.InlineRules.Insert(0, new NugetShieldInlineRule());
+        }
+    }
+}
