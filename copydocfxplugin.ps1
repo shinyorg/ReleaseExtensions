cf5b04388099380c2c8a1d5fc724fae081ec7d0e
Allan Ritchie <allan.ritchie@gmail.com>
2021-12-22 15:10:19 -0500

    Delete copydocfxplugin.ps1

--- copydocfxplugin.ps1
+++
@@ -1,4 +0,0 @@
-# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-New-Item -ItemType Directory -Force -Path ./samples/docfx/simple_template/plugins
-copy ./src/shiny.docfx.extensions/bin/debug/net48/shiny.docfx.extensions.dll ./samples/docfx/simple_template/plugins/
-docfx samples\docfx\docfx.json --serve
